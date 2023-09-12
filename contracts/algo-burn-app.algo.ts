ec734d4e60868693e1e9674b36f66521635b3c02
Joe Polny <joepolny@gmail.com>
2023-09-12 18:14:28 -0400

    initial commit

--- contracts/algo-burn-app.algo.ts
+++ contracts/algo-burn-app.algo.ts
@@ -2,46 +2,17 @@ import { Contract } from '@algorandfoundation/tealscript';
 
 // eslint-disable-next-line no-unused-vars
 class AlgoBurnApp extends Contract {
-   /**
-   * Calculates the sum of two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The sum of a and b
-   */
-  private getSum(a: number, b: number): number {
-    return a + b;
-  }
-
   /**
-   * Calculates the difference between two numbers
+   * Opt the contract into an ASA
    *
-   * @param a
-   * @param b
-   * @returns The difference between a and b.
+   * @param asa The ASA to opt in to
    */
-  private getDifference(a: number, b: number): number {
-    return a >= b ? a - b : b - a;
-  }
-
-  /**
-  * A method that takes two numbers and does either addition or subtraction
-  *
-  * @param a The first number
-  * @param b The second number
-  * @param operation The operation to perform. Can be either 'sum' or 'difference'
-  *
-  * @returns The result of the operation
-  */
-  doMath(a: number, b: number, operation: string): number {
-    let result: number;
-
-    if (operation === 'sum') {
-      result = this.getSum(a, b);
-    } else if (operation === 'difference') {
-      result = this.getDifference(a, b);
-    } else throw Error('Invalid operation');
-
-    return result;
+  optIntoASA(asa: Asset): void {
+    assert(!globals.currentApplicationAddress.hasAsset(asa));
+    sendAssetTransfer({
+      assetReceiver: globals.currentApplicationAddress,
+      xferAsset: asa,
+      assetAmount: 0,
+    });
   }
 }
